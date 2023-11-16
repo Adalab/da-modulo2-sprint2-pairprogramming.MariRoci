996c65ef39181bacd65c685e1903fe13704fddea
mcpperogil <mcperezperogil@gmail.com>
2023-11-16 15:37:14 +0000

    actualizada BD5

--- Pair_Programming_Sprint2_BD5.sql
+++ Pair_Programming_Sprint2_BD5.sql
@@ -26,3 +26,49 @@ INNER JOIN `order_details` ON `orders`.`order_id` = `order_details`.`order_id`
 WHERE `customers`.`country` = "UK"
 GROUP BY `customers`.`company_name`, `Año`;
 
+/*Cuarto ejercicio */
+
+ SELECT `company_name`, `orders`.`order_id`, `order_date`, `country`
+		FROM `customers` 
+		INNER JOIN `orders` 
+		ON `c`.`customer_id` = `o`.`customer_id`;
+ 
+ /*Quinto ejercicio */
+ 
+SELECT `categories`.`category_id`, `categories`.`category_name`, `products`.`product_name`, `products`.`unit_price`*`products`.`units_on_order` AS `Importe_total`
+		FROM `categories`
+		INNER JOIN `products` 
+		ON `categories`.`category_id` = `products`.`category_id`
+		HAVING `Importe_total` >0;
+      
+
+/*Sexto ejercicio */
+
+SELECT `customers`.`company_name`, `orders`.`order_id`, `orders`.`order_date`
+		FROM `customers` AS `c`
+		NATURAL JOIN `orders` AS `o`;
+
+/*Séptimo ejercicio */
+
+SELECT `customers`.`company_name`, COUNT(`orders`.`order_id`) AS total_pedidos, `customers`.`country`
+		FROM `customers` 
+        INNER JOIN `orders` 
+        ON `c`.`customer_id` = `o`.`customer_id`
+        GROUP BY (`o`.`customer_id`)
+        HAVING `c`.`country` = 'UK';
+
+/*Octavo ejercicio */
+
+SELECT `customers`.`company_name`, `orders`.`order_id`, `orders`.`order_date`, `customers`.`country`
+		FROM `customers` 
+		LEFT JOIN `orders` 
+		ON `customers`.`customer_id` = `o`.`customer_id`
+        WHERE `customers`.`country` = 'UK'
+		UNION
+		SELECT `customers`.`company_name`, `orders`.`order_id`, `o`.`order_date`, `customers`.`country`
+		FROM `orders` 
+		LEFT JOIN `customers` 
+		ON `customers`.`customer_id` = `orders`.`customer_id`
+		WHERE `customers`.`country` = 'UK';         
+        
+ 
