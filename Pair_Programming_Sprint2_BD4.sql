eadd4781accb22c4cbcfd9143a75bced22f90d48
mcpperogil <mcperezperogil@gmail.com>
2023-11-16 16:06:49 +0000

    BBDD 4, done

--- Pair_Programming_Sprint2_BD4.sql
+++ Pair_Programming_Sprint2_BD4.sql
@@ -1,5 +1,7 @@
 USE `northwind`;
 
+/*Queries Avanzadas */
+
 /*Primer ejercicio. Productos más barato y más caros */
 SELECT `product_name`, `unit_price` AS `lowestPrice`
 	FROM `products`
@@ -59,3 +61,44 @@ SELECT `product_id`, `product_name`,
 SELECT `employee_id`, COUNT(`order_id`), MAX(`freight`)
   FROM `orders`
   GROUP BY `employee_id`;
+  
+/*Octavo ejercicio. Descartar pedidos sin fechas y ordenarlos*/
+ 
+ SELECT `employee_id`, COUNT(`order_id`) AS `Num_pedidos`, 
+	MAX(`freight`) AS `pedido_mas_pesado`
+	FROM `orders`
+	WHERE `shipped_date` IS NOT NULL
+	GROUP BY `employee_id`;
+
+/*Noveno ejercicio. Número de pedidos por día*/
+
+SELECT	DAY(`order_date`) AS `Dia`, 
+		MONTH(`order_date`) AS `Mes`, 
+        YEAR(`order_date`) AS `Año`, COUNT(`order_id`)
+	FROM `orders`
+	GROUP BY `Dia`, `Mes`, `Año`; 
+	
+/*Décimo ejercicio. Número de pedidos por mes y año*/
+		
+SELECT 	MONTH (`order_date`) AS `Mes`, 
+		YEAR (`order_date`) AS `Año`, 
+        COUNT(`order_id`) AS `numero_de_pedidos`
+	FROM `orders`
+	GROUP BY `Mes`, `Año`;
+    
+/*Undécimo ejercicio. Ciudades con más de 4 empleadas*/
+
+SELECT `city`, COUNT(city) AS `total_empleadas`
+FROM `employees`
+GROUP BY `city`
+HAVING `total_empleadas` >= 4;
+
+/*Duodécimo ejercicio. Nueva columna monetaria*/
+
+SELECT `order_id`, SUM(`unit_price`*`quantity`) AS `precio_total`,
+CASE
+		WHEN SUM(`unit_price`*`quantity`)> 2000 THEN 'Alto'
+		ELSE 'Bajo'
+	END AS `categoria_pedidos`
+FROM `order_details`
+GROUP BY `order_id`;
